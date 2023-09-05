2d5156f9eeb880d6e3806dbfb5e43a4775b265ce
Aniket Datta <datta.aniket7@gmail.com>
2023-09-06 00:41:13 +0530

    refactoring-from-classes

--- singleton.js
+++ singleton.js
@@ -17,25 +17,34 @@
 // console.log(bag.getBag === bag2.getBag);
 // console.log(bag);
 
-let instance;
+// let instance;
 
-class ShoppingBag {
-  constructor() {
-    if (instance) {
-      throw new Error("This instance already exists");
-    }
-    this.bag = [];
-    instance = this;
-  }
-  getBag() {
-    console.log(this.bag);
-  }
-  addItem(item) {
-    this.bag.push(item);
-  }
-}
+// class ShoppingBag {
+//   constructor() {
+//     if (instance) {
+//       throw new Error("This instance already exists");
+//     }
+//     this.bag = [];
+//     instance = this;
+//   }
+//   getBag() {
+//     console.log(this.bag);
+//   }
+//   addItem(item) {
+//     this.bag.push(item);
+//   }
+// }
 
-const singletonShopping = Object.freeze(new ShoppingBag());
+let shopping = [];
+
+const ShoppingBag = {
+  getBag: () => console.log(`This item has ${shopping.length}`),
+  addItem: (item) => shopping.push(item),
+};
+
+// const singletonShopping = Object.freeze(new ShoppingBag());
+
+const singletonShopping = Object.freeze(ShoppingBag);
 
 // singletonShopping.bag = [];
 
